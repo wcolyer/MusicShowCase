2ac88f16665ff075405a2eb575c41177fcb7b5f9
wcolyer <wallace@ecolyer.com>
2025-08-08 09:27:37 -0400

    Include MusicShowCaseApp contents (remove nested git)

---
+++ MusicShowCaseApp/MusicShowCaseApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicShowCaseApp
+//
+//  Created by Wallace Colyer on 8/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicShowCaseApp/MusicShowCaseApp/MusicShowCaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicShowCaseAppApp.swift
+//  MusicShowCaseApp
+//
+//  Created by Wallace Colyer on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicShowCaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
